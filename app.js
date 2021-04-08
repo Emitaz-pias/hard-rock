1a7c1331a90f90fe91d2439929ac638b1736247d
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-04-08 07:24:11 +0600

    lyrics display

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 searchSong = () => {
   const searchedText = document.getElementById("search-field").value;
+  searchedText.value=" "
   const url = `https://api.lyrics.ovh/suggest/:${searchedText}`;
-fetch(url)
-.then(res=>res.json())
-.then(data=>showSong(data.data));
-showSong=songs=>{
-  const songContainer = document.getElementById("song-container")
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showSong(data.data));
+  showSong = (songs) => {
+    const songContainer = document.getElementById("song-container");
+    songContainer.innerHTML = "";
 
-  songs.forEach(song => {
-    const createSongDiv = document.createElement("div")
-createSongDiv.className="single-result row align-items-center my-3 p-3"
-const createSongDetails = `
+    songs.forEach((song) => {
+      const createSongDiv = document.createElement("div");
+      createSongDiv.className = "single-result row align-items-center my-3 p-3";
+      const createSongDetails = `
 <div class="col-md-9">
 <h3 class="lyrics-name">${song.title}</h3>
 <p class="author lead">Album by <span>${song.artist.name}</span></p>
@@ -22,15 +24,22 @@ const createSongDetails = `
 <div class="col-md-3 text-md-right text-center">
 <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
 </div>
-`
-createSongDiv.innerHTML=createSongDetails;
-songContainer.appendChild(createSongDiv)
-console.log(song);
-  });
-
-}
+`;
+      createSongDiv.innerHTML = createSongDetails;
+      songContainer.appendChild(createSongDiv);
+      // console.log(song);
+    });
+  };
 };
-
-const getLyrics =(artist,songName)=>{
-  console.log(artist,songName);
-}
+// get lyrics
+const getLyrics = async (artist, songName) => {
+  const url = `https://api.lyrics.ovh/v1/${artist}/${songName}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  displayLyrics(data);
+};
+// display lyrics
+displayLyrics = (lyrics) => {
+  const getLyricsDiv = document.getElementById("lyrics-div");
+  getLyricsDiv.innerText = lyrics.lyrics;
+};
